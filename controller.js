6cdf49e0ef1f7cfd10f8334e6bcbdff4c7633102
Your Name <you@example.com>
2021-11-25 10:58:49 +0700

    uwis

--- controller.js
+++ controller.js
@@ -10,7 +10,7 @@ exports.index = function(req,res){
 
 //menampilkan semua data ina219
 exports.tampilsemuadata = function(req,res){
-    connection.query('SELECT * FROM ina219', function(error, rows, fields){
+    connection.query('SELECT * FROM data', function(error, rows, fields){
         if(error){
             connection.log(error);
         }else{
@@ -23,7 +23,7 @@ exports.tampilsemuadata = function(req,res){
 //menampilkan data ina219 berdasarkan id
 exports.tampilberdasarid = function(req,res){
     let id = req.params.id;
-    connection.query('SELECT * FROM ina219 WHERE ID = ?', [id], function(error, rows, fields){
+    connection.query('SELECT * FROM data WHERE ID = ?', [id], function(error, rows, fields){
         if (error){
             console.log(error);
         }else{
@@ -34,12 +34,12 @@ exports.tampilberdasarid = function(req,res){
 
 //tambah data ina219
 exports.tambahdata = function (req,res){
-    var current = req.body.current;
-    var voltage = req.body.voltage;
+    var currentdc = req.body.currentdc;
+    var voltagedc = req.body.voltagedc;
     
     // var time = req.body.time;
 
-    connection.query('INSERT INTO ina219 (current,voltage) VALUES (?,?)',[current,voltage],function(error, rows, fields){
+    connection.query('INSERT INTO data (currentdc,voltagedc) VALUES (?,?)',[currentdc,voltagedc],function(error, rows, fields){
         if (error){
          console.log(error);   
 
@@ -52,10 +52,10 @@ exports.tambahdata = function (req,res){
 // megubah data berdasarkan idnya
     exports.ubahdata = function (req,res){
         var ID = req.body.ID;
-        var current = req.body.current;
-        var voltage = req.body.voltage;
+        var currentdc = req.body.currentdc;
+        var voltagedc = req.body.voltagedc;
 
-        connection.query('UPDATE ina219 SET current=?, voltage=? WHERE ID=?',[current,voltage,ID],function(error, rows, fields){
+        connection.query('UPDATE data SET currentdc=?, voltagedc=? WHERE ID=?',[currentdc,voltagedc,ID],function(error, rows, fields){
             if (error){
                 console.log(error);   
        
@@ -69,7 +69,7 @@ exports.tambahdata = function (req,res){
 exports.hapusdata = function(req,res){
     var ID = req.body.ID;
 
-    connection.query('DELETE FROM ina219 WHERE ID=?',[ID],function(error, rows, fields){
+    connection.query('DELETE FROM data WHERE ID=?',[ID],function(error, rows, fields){
         if (error){
             console.log(error);   
    
--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 const express = require('express');
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:3000"
+  };
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-//pangil routes
-var routes = require('./routes');
-routes(app);
+// //pangil routes
+// var routes = require('./routes');
+// routes(app);
 
-app.listen(3000, () => {
-    console.log(`Server started on port`);
+// app.listen(3000, () => {
+//     console.log(`Server started on port`);
+// });
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to application." });
+  });
+  
+  require("./routes.js")(app);
+
+  // set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
