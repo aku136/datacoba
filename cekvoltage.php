7e142bc9f7d22a59b12dd098b7b9169059af411a
Your Name <you@example.com>
2021-12-09 09:03:49 +0700

    yes

--- cekvoltage.php
+++ cekvoltage.php
@@ -1,6 +1,6 @@
 <?php
 //koneksi
-$konek = mysqli_connect("dbkelompoka.gmedia.bz", "gmedia_magangb", "indo1945!merdeka", "gmedia_magangb");
+$konek = mysqli_connect("3306",  "mysql", "dbkelompoka.gmedia.bz", "gmedia_magangb", "indo1945!merdeka", "gmedia_magangb");
 
 //manggil data terakhir
 $sql = mysqli_query($konek, "select * from data order by id desc");
