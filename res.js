f5d58bc8b29a9491e6c18bda92f8825519b5795a
Your Name <you@example.com>
2021-12-23 13:45:14 +0700

    y

--- res.js
+++ res.js
@@ -1,11 +1,11 @@
 "use strict";
 
 exports.ok = function (values, res) {
-  // var data = {
-  //   status: 200,
-  //   values: values,
-  // };
+  var data = {
+    status: 200,
+    values: values,
+  };
 
-  res.json(values);
+  res.json(data);
   res.end();
 };
