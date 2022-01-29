2eeb34485ed4735c7466537489f2bde1e7859aaa
Your Name <you@example.com>
2022-01-29 09:34:11 +0700

    y

--- routes.js
+++ routes.js
@@ -12,8 +12,6 @@ module.exports = function (app) {
 
   app.route("/users").get(jsonku.tampilusers);
 
-  app.route("/contoh").get(jsonku.tampilcontoh);
-
   app.route("/tampilbaru").get(jsonku.dataterbaru);
 
   app.route("/tampil/:id").get(jsonku.tampilberdasarid);
